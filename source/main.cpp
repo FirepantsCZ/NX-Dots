d45e1fc68a4e5c9b2e33c3c9d93ef29032f70be4
Paolo Zisa <paolozisa24@gmail.com>
2025-09-10 15:57:15 +0200

    initial setup with custom renderer

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,10 @@
 #include <tesla.hpp>    // The Tesla Header
 
 
+void renderDots(tsl::gfx::Renderer* r, s32 x, s32 y, s32 w, s32 h) {
+}
+
+
 class GuiTest : public tsl::Gui {
 public:
     GuiTest() { }
@@ -11,16 +15,19 @@ public:
     virtual tsl::elm::Element* createUI() override {
         // A OverlayFrame is the base element every overlay consists of. This will draw the default Title and Subtitle.
         // If you need more information in the header or want to change it's look, use a HeaderOverlayFrame.
-        auto frame = new tsl::elm::OverlayFrame("Tesla Example", "v1.3.1");
+        // auto frame = new tsl::elm::OverlayFrame("Tesla Example", "v1.3.1");
+        auto frame = new tsl::elm::HeaderOverlayFrame();
+
+	auto drawer = new tsl::elm::CustomDrawer(renderDots);
 
         // A list that can contain sub elements and handles scrolling
-        auto list = new tsl::elm::List();
+        // auto list = new tsl::elm::List();
 
         // Create and add a new list item to the list
-        list->addItem(new tsl::elm::ListItem("Default List Item"));
+        // list->addItem(new tsl::elm::ListItem("Default List Item"));
 
         // Add the list to the frame for it to be drawn
-        frame->setContent(list);
+        // frame->setContent(list);
 
         // Return the frame to have it become the top level element of this Gui
         return frame;
